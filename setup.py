974252c0dc57e0c3bbd27df935a7a8a061b53d1b
John A.R. Williams <j.a.r.williams@aston.ac.uk>
2023-02-25 15:45:35 +0000

    Add template-template.xlsx to package data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyAutoMark',
-    version='0.3.0',  
+    version='0.3.1',  
     python_requires='>3.6.0',  
     description='Automated marking of student electronic submissions',
     url='https://github.com/willijar/pyAutoMark',
@@ -11,6 +11,7 @@ setuptools.setup(
     license='GPL-3.0-Only',
     packages=['pyam','pyam.fixtures','pyam.cmd'],
     package_dir={'pyam':'pyam'},
+    package_data={'pyam': ['*.xlsx']},
     install_requires=['openpyxl>=3.0',
                       'pytest>=7.2.1',                     
                       ],
