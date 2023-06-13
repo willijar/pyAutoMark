1dc943bd668f2b6220200ef43bcb8cf3cf4653e5
John A.R. Williams <j.a.r.williams@aston.ac.uk>
2023-06-13 13:01:26 +0100

    fix: no PYAM_TEST_VALUES case check

--- pyam/fixtures/vhdl.py
+++ pyam/fixtures/vhdl.py
@@ -329,7 +329,7 @@ def pytest_collect_file(parent, path) -> List:
 
     Tests are recognised as symbols matching "TEST\_[A-Z0-9\_]+"
 
-    A test timeout may be specified using #DEFINE PYAM_TIMEOUT <float>
+    A test timeout may be specified using -- PYAM_TIMEOUT <float>
 
     Returns:
         List of  tuples of filepath and a list of tests declarations.
@@ -392,7 +392,7 @@ class VHDLTestFile(pytest.File):
         Overridden collect method to collect the results from a vhdl file.
         If PYAM_TEST_VALUEs are specified these will be used
         """
-        if self.test_values is not None:
+        if self.test_values:
             for test in self.test_values:
                 yield VHDLTestItem.from_parent(name=test,
                                                parent=self,
