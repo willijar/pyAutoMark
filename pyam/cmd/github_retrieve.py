db81ca08939e23377c71172e31b3fe9361b97676
John A.R. Williams <j.a.r.williams@aston.ac.uk>
2023-11-22 09:38:12 +0000

    Simplify and tidy git/github interactions

--- pyam/cmd/github_retrieve.py
+++ pyam/cmd/github_retrieve.py
@@ -21,14 +21,13 @@ def add_args(parser=argparse.ArgumentParser(description=__doc__)):
                         help="If specified will checkout out last repository before given date - format Y/M/D H:M. This may be a headless checkout."
                         )
     parser.add_argument('--branch', 
-                        default=CONFIG.get("github.branch"),
-                        help="If specified checkout repositories to specified branch - defaults to github.branch"
+                        default=None,
+                        help="If specified checkout repositories to specified branch"
                         )
-    parser.add_argument(
-        '-s',
-        '--students',
-        nargs="*",
-        help='Names of specific student for which tests are to be run')
+    parser.add_argument('-s',
+                        '--students',
+                        nargs="*",
+                        help='Names of specific student for which tests are to be run')
     parser.add_argument("--no-reset", 
                         action="store_true",
                         help="If specified then local repositories won't be reset before a pull.")
@@ -53,9 +52,7 @@ def main(args=None):
     students = cohort.students(args.students)
     submission_dates = {}
     for student in students:
-        if not args.no_reset:
-            student.checkout(until=None,branch=args.branch)
-        student.github_retrieve(reset=not(args.no_reset))
+        student.github_retrieve(reset=not(args.no_reset),branch=args.branch) # yeh double negative
         submission_dates[student.username] = student.github_lastcommit()
         if args.until:
             student.checkout(until=args.until,branch=args.branch)
